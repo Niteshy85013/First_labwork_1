d1f3d32a939436de1ba57289ce40d8771f712721
Nitesh <niteshyadav85013@gmail.com>
2021-03-04 09:57:55 +0545

    initiate

---
+++  secondsec.py/labwork1/calculation.py
@@ -0,0 +1,23 @@
+# given the integer N=the number of minutes that is passed since midnight =how many hours and minutes are
+# displayed on the 24h digital clock?the program should print two number:the number of hours(between 0 and 23)and the
+# number of minutes between(0 and 59).for example if N=150,then 150 minutes have passed since midnight i.e now
+# is 2:30 am so,the program should print 2:30.
+
+N=int(input("enter the minutes passed since midnight :"))
+hours=(N//60)
+minutes=(N%60)
+print(f"the hour is {hours}")
+print(f"the minutes is {minutes}")
+print(f"its{hours}:{minutes} now")
+
+
+
+
+
+
+
+
+
+
+
+
