2fabdbcda3f04ad5501bbbe74709afb6847a8cb4
Niteshy85013 <niteshyadav85013@gmail.com>
2021-04-01 21:41:07 +0545

    Calculation_of_Time

---
+++ First_labwork_1/calculation_of_time.py
@@ -0,0 +1,8 @@
+
+
+N=int(input("enter the minute passed since mid night:"))
+hours=(N//60)
+minutes=(N%60)
+print(f"The hour is {hours}")
+print(f"The minutes is {minutes}")
+print(f"its {hours}:{minutes} now.")
