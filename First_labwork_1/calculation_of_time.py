c2a2f59f4ef37e31dd6160e4c58aedc61237159e
Niteshy85013 <niteshyadav85013@gmail.com>
2021-04-01 21:48:26 +0545

    Calculation_of_Time

--- First_labwork_1/calculation_of_time.py
+++ First_labwork_1/calculation_of_time.py
@@ -1,4 +1,7 @@
-
+'''The given integer N=The number of minutes that is passed since  midnight = how many minutes
+and hours are displayed on the 24h digital clock ? the program should print the two hours
+(between 0 and 23) and number of minutes between (0 and 59) for example  if N=150,then 150 minute
+have passed since midnight i.e  now is  2:30 so, the the program should  print 2:30 '''
 
 N=int(input("enter the minute passed since mid night:"))
 hours=(N//60)
