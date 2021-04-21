36fee045445492826de8e1b44737f541c92b7d5f
Niteshy85013 <niteshyadav85013@gmail.com>
2021-04-21 10:24:28 +0545

    changing logo

--- First_labwork_1/change logo.py
+++ First_labwork_1/change logo.py
@@ -1,6 +1,22 @@
-from tkinter import*
+'''from tkinter import*
 root=Tk()
 root.title('image insertion')
 root.iconbitmap('male.ico')
+root.mainloop()'''
+
+from tkinter import*
+from PIL import ImageTk, Image
+root=Tk()
+root.title('Image Insertion')
+root.iconbitmap('male.ico')
+
+my_image= ImageTk.PhotoImage (Image .open('female.png') )
+my_label=Label(image=my_image)
+my_label.pack()
+
+
+Button_quite=Button(root,text="exit",command=root.quit,width=20)
+Button_quite.pack()
 root.mainloop()
+
 
